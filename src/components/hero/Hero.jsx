12c076d73201799f14b9ebf391af403f0e9e0f2c
12imthi <msdimthi6@gmail.com>
2026-03-30 00:35:03 +0530

    update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './Hero.css'
 import profileImg from '../../assets/banner/profile2.png'
 import toolKnow from '../../assets/banner/tools.png'
+import textAnimation from '../../assets/text_animation/text.mp4'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 function Hero() {
   return (
   <div className="hero">
+     
   <div className="banner">
+<h1><h1><h1></h1></h1></h1> */}
     <img src={profileImg} alt="banner" />
 
     <a 
